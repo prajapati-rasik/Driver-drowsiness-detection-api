7975377afbcf639a18623fef860a59404e63bcca
Rasikkumar Prajapati <rasik.prajapati.prajapati@oracle.com>
2025-03-18 19:52:44 +0530

    model changes

--- model_files/model.py
+++ model_files/model.py
@@ -10,8 +10,10 @@ def predict(l_eye, r_eye):
     rpred = [99]  # for taking class prediction of right eye
     lpred = [99]  # for taking class prediction of left eye
 
-    rpred = model.predict_classes(r_eye)
-    lpred = model.predict_classes(l_eye)
+    rpred = model.predict(r_eye)
+    rpred = np.argmax(rpred, axis=1)
+    lpred = model.predict(l_eye)
+    lpred = np.argmax(lpred, axis=1)
 
     if(rpred[0] == 0 and lpred[0] == 0):  # if both eyes are closed incerement the score
         return True
