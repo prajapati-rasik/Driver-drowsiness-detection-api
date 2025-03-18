79e898777a88eb8607592003f0530dd214d56426
Rasikkumar Prajapati <rasik.prajapati.prajapati@oracle.com>
2025-03-19 00:20:09 +0530

    model path change

--- model_files/model.py
+++ model_files/model.py
@@ -6,7 +6,7 @@ from keras.models import load_model
 def predict(l_eye, r_eye):
 
     # loading our model
-    model = load_model('model_files/modell.h5')
+    model = load_model('./modell.h5')
     rpred = [99]  # for taking class prediction of right eye
     lpred = [99]  # for taking class prediction of left eye
 
